d306c7c9248ad28dba5357120d59774ee1fb90b5
Mathieu Brujan <mathieu.brujan@epitech.eu>
2023-01-29 16:17:09 +0100

    feat: add two canvas that represents what is cropped and what's not

--- src/crop/crop.js
+++ src/crop/crop.js
@@ -7,6 +7,18 @@ let cropper = document.getElementById("cropper");
 if (imageData == null)
     location.href = "http://localhost:8080"
 
+preview.onload = () => {
+    background.width = preview.width;
+    background.height = preview.height;
+    cropper.width = preview.width / 2;
+    cropper.height = preview.width / 2;
+    let backgroundContext = background.getContext('2d');
+    let backgroundRect = background.getBoundingClientRect()
+    let cropperRect = cropper.getBoundingClientRect()
+    backgroundContext.fillRect(0, 0, preview.width, preview.height);
+    backgroundContext.clearRect(cropperRect.x - backgroundRect.x, cropperRect.y - backgroundRect.y, cropperRect.width, cropperRect.height)
+}
+
 preview.src = imageData
 preview.style.display = "block";
 
